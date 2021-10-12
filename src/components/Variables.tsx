99df2905e0ca6e283ce2bf191278799fa40442cb
pechavarriaa <pechavarriaa@gmail.com>
2021-10-12 13:36:18 -0600

    change to columns

--- src/components/Variables.tsx
+++ src/components/Variables.tsx
@@ -7,11 +7,10 @@ export type VariablesProps = {
 
 export const Variables: FC<VariablesProps> = ({ networkVars }) => {
     const theme = useTheme();
+    const networkVarsBuckets = getVarsInBucketsOf10(networkVars);
     return (
         <Stack styles={{ root: { padding: '2px' } }}>
-            <Stack.Item
-                styles={{ root: { overflow: 'hidden', marginBottom: '5px' } }}
-            >
+            <Stack.Item styles={{ root: { marginBottom: '5px' } }}>
                 <Text
                     variant="mediumPlus"
                     styles={{
@@ -26,28 +25,38 @@ export const Variables: FC<VariablesProps> = ({ networkVars }) => {
             <Stack.Item
                 styles={{
                     root: {
-                        overflowY: 'scroll',
-                        '::-webkit-scrollbar': { width: '5px' },
-                        '::-webkit-scrollbar-track': { background: '#f1f1f1' },
-                        '::-webkit-scrollbar-thumb': {
-                            background: '#888',
-                        },
-                        '::-webkit-scrollbar-thumb:hover': {
-                            background: '#555',
-                        },
-                        maxHeight: '150px',
-                        minWidth: '100px',
                         backgroundColor: theme.palette.neutralLight,
                         borderRadius: '6px',
+                        paddingRight:'20px'
                     },
                 }}
             >
-                <ul>
-                    {networkVars.map((networkVar, i) => (
-                        <li key={i}>{networkVar}</li>
+                <Stack horizontal>
+                    {networkVarsBuckets.map((networkVars, i) => (
+                        <Stack.Item key={i}>
+                            <ul>
+                                {networkVars.map((networkVar, j) => (
+                                    <li key={j}>{networkVar}</li>
+                                ))}
+                            </ul>
+                        </Stack.Item>
                     ))}
-                </ul>
+                </Stack>
             </Stack.Item>
         </Stack>
     );
+};
+
+const getVarsInBucketsOf10 = (
+    networkVars: Array<string>
+): Array<Array<string>> => {
+    let arrayOfSize10Vars = [];
+    for (let x = 0; x < networkVars.length; x += 10) {
+        let tempArr = [];
+        for (let y = x; y < x + 10 && y < networkVars.length; y++) {
+            tempArr.push(networkVars[y]);
+        }
+        arrayOfSize10Vars.push(tempArr);
+    }
+    return arrayOfSize10Vars;
 };
