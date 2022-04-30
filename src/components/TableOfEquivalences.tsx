071ada26d0634060771c1dea2921647b19e510fb
pechavarriaa <pechavarriaa@gmail.com>
2022-04-29 18:24:03 -0600

    added image in table

--- src/components/TableOfEquivalences.tsx
+++ src/components/TableOfEquivalences.tsx
@@ -4,13 +4,14 @@ import {
     enumOfBAR,
     BooleanAlgebraRelations,
 } from '../definitions/BooleanAlgebraRelations';
+import { relationImages } from '../definitions/relationsImages';
 
 export const TableOfEquivalences: FC = () => {
     const theme = useTheme();
     const tableCellStyle: CSSProperties = {
         borderCollapse: 'collapse',
         border: `2px solid ${theme.palette.tealLight}`,
-        padding: '5px'
+        padding: '5px',
     };
     return (
         <table
@@ -28,17 +29,23 @@ export const TableOfEquivalences: FC = () => {
                 <tr>
                     <th style={tableCellStyle}>No.</th>
                     <th style={tableCellStyle}>Relation</th>
+                    <th style={tableCellStyle}>Image</th>
                 </tr>
             </thead>
             <tbody>
-                {Object.entries(enumOfBAR).map(([BarAsString, BarAsNumber], i) => (
-                    <tr key={i}>
-                        <td style={tableCellStyle}>{BarAsNumber}</td>
-                        <td style={tableCellStyle}>
-                            {BooleanAlgebraRelations[BarAsString]}
-                        </td>
-                    </tr>
-                ))}
+                {Object.entries(enumOfBAR).map(
+                    ([BarAsString, BarAsNumber], i) => (
+                        <tr key={i}>
+                            <td style={tableCellStyle}>{BarAsNumber}</td>
+                            <td style={tableCellStyle}>
+                                {BooleanAlgebraRelations[BarAsString]}
+                            </td>{' '}
+                            <td style={tableCellStyle}>
+                               <img src={relationImages[BarAsString]} alt={BarAsString} style={{height: '24px'}} />
+                            </td>
+                        </tr>
+                    )
+                )}
             </tbody>
         </table>
     );
